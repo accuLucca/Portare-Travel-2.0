7ad0175459c08f3aa79a3dce982dfe748aee4421
Lucca Bulara <43175678+accuLucca@users.noreply.github.com>
2025-10-23 18:53:30 -0300

    update

--- js/egyptData.js
+++ js/egyptData.js
@@ -1,24 +1,4 @@
 export const egyptData = [
-    // EGITO MÁGICO COM CRUZEIRO PELO NILO COMPARTILHADO - 7 DIAS
-    {
-        id: 202,
-        name: "EGITO MÁGICO COM CRUZEIRO PELO NILO - 7 DIAS",
-        description: "Explore o Egito em 7 dias com um cruzeiro compartilhado pelo Nilo. Visite templos antigos, sítios arqueológicos e desfrute da paisagem do rio. Inclui acomodação e algumas refeições. (Descrição detalhada e preço a serem confirmados)", // Placeholder description
-        price: 0.00, // Placeholder price - UPDATE THIS
-        imageUrl: "img/egito/rionilo.jpg", // Placeholder image
-        category: "Tour em grupo",
-        pdfUrl: "img/pdf/EgitoCompartilhado7dias.pdf"
-    },
-    // EGITO COMPLETO COM MAR VERMELHO COMPARTILHADO - 10 DIAS
-    {
-        id: 203,
-        name: "EGITO COMPLETO COM MAR VERMELHO - 10 DIAS",
-        description: "Viva uma jornada inesquecível de 10 dias pelo Egito com guia em português. Explore as Pirâmides de Gizé, navegue pelo Rio Nilo em um cruzeiro com pensão completa e relaxe nas praias de Hurghada, no Mar Vermelho. Uma combinação perfeita de história, cultura e lazer. (Descrição detalhada e preço a serem confirmados)", // Updated description
-        price: 0.00, // Placeholder price - UPDATE THIS
-        imageUrl: "img/egito/Camelos.jpg", // Placeholder image
-        category: "Tour em grupo",
-        pdfUrl: "img/pdf/EgitoCompartilhado10dias.pdf" 
-    },
     // EGITO HISTÓRICO C/ CRUZEIRO PRIVATIVO 8 DIAS
     {
         id: 204,
